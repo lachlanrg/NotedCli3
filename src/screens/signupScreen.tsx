8f7b3100492b221f4e3d648490d926f08093c3dc
Lachlan Greig <lachlanrgreig@gmail.com>
2024-04-07 11:00:35 +1000

    Dev-26-user-auth, added amplify auth for signin, signup, singout, profilescreen

--- src/screens/signupScreen.tsx
+++ src/screens/signupScreen.tsx
@@ -1,56 +1,130 @@
 // SignUpScreen.tsx
 import * as React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+import { signUp } from 'aws-amplify/auth';
+import { autoSignIn } from 'aws-amplify/auth';
+import { signIn } from 'aws-amplify/auth';
+import { confirmSignUp, type ConfirmSignUpInput } from 'aws-amplify/auth';
+
 
 type SignUpScreenProps = {
   navigation: NativeStackNavigationProp<any>;
+};
+
+type SignUpParameters = {
+  username: string;
+  password: string;
+  email: string;
 };
 
 const SignUpScreen: React.FC<SignUpScreenProps> = ({ navigation }) => {
   const [username, setUsername] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [confirmationCode, setConfirmationCode] = React.useState('');
+  const [isConfirmationStep, setIsConfirmationStep] = React.useState(false);
 
-  const handleSignUp = () => {
-    // Perform signup logic here
-    // For simplicity, we'll just log the values for now
-    console.log('Username:', username);
-    console.log('Email:', email);
-    console.log('Password:', password);
+  const handleSignUp = async () => {
+    try {
+      console.log('Attempting sign up with username: ', username, 'email: ', email, 'and password:', password);
 
-    // Navigate to the Main Screen
-    navigation.navigate('Main');
+      await signUp({
+        username,
+        password,
+        options: {
+          userAttributes: {
+            email,
+          },
+          autoSignIn: true,
+        },
+      });
+      
+      setIsConfirmationStep(true);
+
+      console.log('Sign up successful for user:', username);
+
+    } catch (error: any) {
+      console.error('Error signing up:', error);
+      Alert.alert('Error signing up:', error.message);
+    }
   };
+
+  const handleConfirmation = async () => {
+    try {
+      console.log('Confirming sign up with username: ', username, 'and confirmation code:', confirmationCode);
+
+      await confirmSignUp({
+        username,
+        confirmationCode,
+      });
+
+      await handleAutoSignIn();
+      navigation.navigate('Main');
+
+      console.log('Sign up confirmed for user:', username);
+
+    } catch (error: any) {
+      console.error('Error confirming sign up:', error);
+      Alert.alert('Error confirming sign up:', error.message);
+    }
+  };
+
+  async function handleAutoSignIn() {
+    try {
+      await autoSignIn();
+      // handle sign-in steps
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Sign Up</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        autoCapitalize="none"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-      />
-      <Button title="Sign Up" onPress={handleSignUp} />
-      <Button title="Back to Login" onPress={() => navigation.navigate('Login')}/>
+      {isConfirmationStep ? (
+        <>
+          <Text style={styles.title}>Confirm Sign Up</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Confirmation Code"
+            value={confirmationCode}
+            onChangeText={setConfirmationCode}
+            keyboardType="numeric"
+            autoCapitalize="none"
+          />
+          <Button title="Confirm Sign Up" onPress={handleConfirmation}/>
+        </>
+      ) : (
+        <>
+          <Text style={styles.title}>Sign Up</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={setPassword}
+            autoCapitalize="none"
+          />
+          <Button title="Sign Up" onPress={handleSignUp}/>
+          <Button title="Back to Login" onPress={() => navigation.navigate('Login')}/>
+        </>
+      )}
     </View>
   );
 };
