2a0f6788131a081789e4fddfb3ad9986982a6b92
Lachlan Greig <lachlanrgreig@gmail.com>
2024-05-16 18:31:08 +1000

    DEV-38-awsauth, added changes, nothing too progressive

--- src/components/types.tsx
+++ src/components/types.tsx
@@ -1,20 +1,30 @@
 // src/types.ts
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Album } from "../screens/searchScreen"
 
 export type RootStackParamList = {
   Home: undefined;
   Profile: undefined;
   CreatePostTab: undefined;
-  Search: undefined; // Add Search to the RootStackParamList
+  // Search: undefined;
+  Search: { screen: string }; 
+  // SearchScreenStack: { screen: string }; 
+  // SearchScreenStack: {
+  //   AlbumDetail: undefined;
+  // };
+  // AlbumDetail: undefined;
+  // Album: undefined;
+
+  // AlbumDetailsStack: { screen: string; params: { album: Album } }; // Update the Main type
+};
+
+export type SearchScreenStackParamList = {
+  Search: undefined;
+  AlbumDetail: { albumId: string }; // Define any parameters the AlbumDetail screen might need
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
   NativeStackScreenProps<RootStackParamList, Screen>;
 
-  // This is only used for the AlbumComponent.tsx
-  export type Album = {
-    id: string;
-    name: string;
-    artist: string;
-    imageUrl: string;
-  };
+export type SearchScreenStackScreenProps<Screen extends keyof SearchScreenStackParamList> =
+  NativeStackScreenProps<SearchScreenStackParamList, Screen>;
---
+++ src/stacks/SearchStack.tsx
@@ -0,0 +1,18 @@
+// // ../stacks/SearchStack.tsx
+// import React from 'react';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import SearchScreen from '../screens/searchScreen';
+// import AlbumDetailsScreen from '../screens/albumDetailsScreen';
+
+// const Stack = createStackNavigator();
+
+// function SearchStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Search" component={SearchScreen} />
+//       <Stack.Screen name="AlbumDetails" component={AlbumDetailsScreen} />
+//     </Stack.Navigator>
+//   );
+// }
+
+// export default SearchStack;
